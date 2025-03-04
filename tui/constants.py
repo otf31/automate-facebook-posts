c34f002b0298b0388068245fa00643bae14c7519
otf31 <otf31.351663@gmail.com>
2025-03-03 19:14:04 -0500

    [tui] ♻️ os chrome binary path constant

--- tui/constants.py
+++ tui/constants.py
@@ -1,16 +1,23 @@
+import platform
 from typing import Literal
+
+os = platform.system()
 
 AUTHOR = "otf31"
 EMAIL = "otf31.351663@gmail.com"
 API_URL = "https://automate-facebook-posts-back.onrender.com/"
 FACEBOOK_URL = "https://www.facebook.com"
-CHROME_BINARY_PATH = "/opt/google/chrome/google-chrome"
+CHROME_BINARY_PATH = (
+    r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    if os == "Windows"
+    else "/opt/google/chrome/google-chrome"
+)
 APP_NAME = "autofbpost"
 CONFIG_FILE = "config.json"
 LOG_FILE = "log.csv"
 CONFIG_KEYS_TYPES = Literal["CHROME_BINARY_PATH", "POSTS_FOLDER_PATH", "HEADLESS"]
 DEFAULT_CONFIG = {
-    "CHROME_BINARY_PATH": "/opt/google/chrome/google-chrome",
+    "CHROME_BINARY_PATH": CHROME_BINARY_PATH,
     "POSTS_FOLDER_PATH": "",
     "HEADLESS": False,
 }
